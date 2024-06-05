3bfdb36c70bd193565b5b19bc6d90d948cb22486
enjoy developer <kh1012@midasit.com>
2024-06-05 20:22:33 +0900

    update!

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,50 @@
-import React from "react";
-import { useNavigate, BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Fragment } from "react";
 import RjsfTester from "./lib/rjsf/Tester";
 import MoauiTester from './lib/moaui/Tester';
 import moa from '@midasit-dev/moaui';
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        background: "linear-gradient(rgba(211, 211, 211, .9) 30%, rgba(255, 142, 83, .4) 90%)",
-      }}
-    >
-      <moa.GuideBox spacing={2} padding={5} width="100%" center>
-        <moa.Typography variant="h1" size="medium">
-          SchemaForms Testers
-        </moa.Typography>
-        <moa.Panel>
-          <moa.GuideBox spacing={1}>
-            <moa.Button
-              onClick={() => navigate("rjsf")}
-              color="negative"
-              width="100%"
-            >
-              React-JsonSchema-Form tester
-            </moa.Button>
-
-            <moa.Button
-              onClick={() => navigate("moaui")}
-              color="negative"
-              width="100%"
-            >
-              moaui tester
-            </moa.Button>
-          </moa.GuideBox>
-        </moa.Panel>
-      </moa.GuideBox>
-    </div>
-  );
-};
-
 const App = () => {
+	const [selectName, setSelectName] = React.useState("home");
+
 	return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="rjsf" element={<RjsfTester />} />
-        <Route path="moaui" element={<MoauiTester />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+		<Fragment>
+			{selectName === "home" && (
+				<div
+					style={{
+						width: "100%",
+						height: "100vh",
+						background: "linear-gradient(rgba(211, 211, 211, .9) 30%, rgba(255, 142, 83, .4) 90%)",
+					}}
+				>
+					<moa.GuideBox spacing={2} padding={5} width="100%" center>
+						<moa.Typography variant="h1" size="medium">
+							SchemaForms Testers
+						</moa.Typography>
+						<moa.Panel>
+							<moa.GuideBox spacing={1}>
+								<moa.Button
+									onClick={() => setSelectName("rjsf")}
+									color="negative"
+									width="100%"
+								>
+									React-JsonSchema-Form tester
+								</moa.Button>
+		
+								<moa.Button
+									onClick={() => setSelectName("moaui")}
+									color="negative"
+									width="100%"
+								>
+									moaui tester
+								</moa.Button>
+							</moa.GuideBox>
+						</moa.Panel>
+					</moa.GuideBox>
+				</div>
+			)}
+			{selectName === "rjsf" && <RjsfTester />}
+			{selectName === "moaui" && <MoauiTester />}
+		</Fragment>
   );
 };
 
